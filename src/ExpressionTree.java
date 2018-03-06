a401264eccb114e5b7cf38cf0d28d03d20f3daf4
Aidan <Aidan@Aidan-PC>
2018-03-05 23:05:00 -0500

    There we go

--- src/ExpressionTree.java
+++ src/ExpressionTree.java
@@ -1,72 +1,124 @@
+
+
 import java.util.Stack;
 
 public class ExpressionTree extends TreeNode implements Expressions{
-	
-	
-	ExpressionTree(String str) {
-		super(str);
-		
-		
+
+
+
+	ExpressionTree(Object v){
+
+		super(v);
+
+		TreeNode n = buildTree(null);
+
+		this.setLeft(n.getLeft());
+
+		this.setRight(n.getRight());
+
+		this.setValue(n.getValue());
+
 	}
 
+
+
 	@Override
-	public TreeNode buildTree(String[] exp) {
-		Stack c = new Stack<String>();
-		TreeNode t = null;
-		TreeNode r;
-		TreeNode l;
-		//TreeNode t = new TreeNode(exp[exp.length-1]);
+
+	public java.util.HashMap.TreeNode buildTree(String[] exp) {
+
+		Stack c =new Stack<TreeNode>();
+		TreeNode t;
 		for(int i = 0; i < exp.length-1; i++) {
 			if(exp[i] != "*" && exp[i] != "+" ) {
-				c.push(exp[i]);
+				c.push(new TreeNode(exp[i]));
 			}else {
-				r = new TreeNode(c.pop());
-				l = new TreeNode(c.pop());
-				t = new TreeNode(exp[i], l, r);
+				t = new TreeNode(exp[i]);
 				c.push(t);
 			}
 		}
-		
-		return t;
+		return c.pop();
+
 	}
 
+
+
 	@Override
+
 	public int evalTree() {
-		if(this.getLeft()==null) {
-			return (int) e.getValue();
-		}
-		
-		
-		
-		
-		
-		
-		
-		
+		int a = 0;
+		return evalTree(a);
+
 	}
 
+	
+
+	public int evalTree(int sum) {
+
+		String a = "";
+		String b = "";
+
+		if(this.getLeft().getLeft() == null) {
+			a  = (String) this.getLeft().getValue();
+			b  = (String) this.getRight().getValue();
+			return Integer.parseInt(a + this.getValue() + b);
+
+		}else {
+			this.getLeft().evalTree();
+		}
+
+		if(this.getRight().getRight() == null) {
+			a  = (String) this.getLeft().getValue();
+			b  = (String) this.getRight().getValue();
+			return Integer.parseInt(a + this.getValue() + b);
+		}
+
+		return sum;
+	}
+
+	
+
 	@Override
+
 	public String toPrefixNotation() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+
+
 	@Override
+
 	public String toInfixNotation() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+
+
 	@Override
+
 	public String toPostfixNotation() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+
+
 	@Override
+
 	public int postfixEval(String[] exp) {
-		// TODO Auto-generated method stub
-		return 0;
+		Stack c = new Stack<String>();
+		int r = 0;
+		for(int i = 0; i < exp.length-1; i++) {
+			if(exp[i] != "*" && exp[i] != "+" ) {
+				c.push(exp[i]);
+			}else {
+				r = Integer.parseInt(c.pop() + exp[i] + c.pop());
+				c.push("" + r);
+			}
+		}
+		return Integer.parseInt((String) c.pop());
+
 	}
-	
+
 }
