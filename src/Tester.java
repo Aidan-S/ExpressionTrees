756b133d37b3134f4fa6be2bd5ff65e9dd8d0d5e
scannella <scannella@bishopireton.org>
2018-03-07 07:51:35 -0500

    Actually do the test thing ya dumb

--- src/Tester.java
+++ src/Tester.java
@@ -1,15 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Tester {
 
+	
+	public static PrintWriter outputFile(String fName) {
+			File fileName = new File(fName);
+			PrintWriter output = null;
+			try {
+				output = new PrintWriter(fileName);
+			} catch (FileNotFoundException ex) {
+				System.out.print("Cannot open " + fName + ", it may not exist");
+				return null;
+			}
+			return output;
+	}
+	
+	
+	
+	public static Scanner openWords(String fname, int fnum, PrintWriter out) {
+		File file = new File(fname);
+		Scanner input = null;
+		try {
+			input = new Scanner(file);
+		} catch (FileNotFoundException ex) {
+			System.out.print("Cannot open " + fname + ", it may not exist");
+			return null;
+		}
+		return input;	
+	}
+	
+	
 	public static void main(String[] args) {
-		String [] num = {"2", "5", "+"};
-		TreeNode k = new TreeNode(null);
-		ExpressionTree t = new ExpressionTree(k); 
+		PrintWriter out = outputFile("knapsack.txt");
 		
-		t.buildTree(num);
+		Scanner file = openWords(args[0], 1, out);
+		Scanner keyboard = new Scanner(System.in);
 		
-		System.out.println(t.evalTree());
-		System.out.println(t.toInfixNotation());
+		String s = keyboard.nextLine();
+		
+		if(file == null) {
+			openWords(s, 1, out);
+		}
+		
+				
+		
+		
+		
 
 	}
 
